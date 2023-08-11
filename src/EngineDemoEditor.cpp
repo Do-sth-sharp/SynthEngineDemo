3651d0b75cded037af2bef398c75eb09bec7a25e
無常 <3142324836@qq.com>
2023-08-12 00:02:08 +0800

    Update EngineDemoEditor

--- src/EngineDemoEditor.cpp
+++ src/EngineDemoEditor.cpp
@@ -1,4 +1,57 @@
 #include "EngineDemoEditor.h"
 
 EngineDemoEditor::EngineDemoEditor(juce::AudioProcessor& processor)
-	: AudioProcessorEditor(processor) {}
+	: AudioProcessorEditor(processor) {
+	/** Info Editor */
+	this->infoEditor = std::make_unique<juce::TextEditor>();
+	this->infoEditor->setMultiLine(true);
+	this->infoEditor->setReadOnly(true);
+	this->infoEditor->setWantsKeyboardFocus(false);
+	this->infoEditor->setTabKeyUsedAsCharacter(true);
+	this->infoEditor->setTextToShowWhenEmpty("No Info",
+		juce::LookAndFeel::getDefaultLookAndFeel().findColour(
+			juce::TextEditor::ColourIds::textColourId));
+	this->addAndMakeVisible(this->infoEditor.get());
+
+	/** Min Size */
+	this->setResizeLimits(480, 270, INT_MAX, INT_MIN);
+	this->setSize(480, 270);
+}
+
+void EngineDemoEditor::resized() {
+	/** Content Area */
+	auto area = this->getContentArea();
+	bool contentVertical = area.getHeight() >= area.getWidth();
+
+	/** Info Size */
+	juce::Rectangle<int> infoArea = contentVertical
+		? area.withTrimmedTop(area.getHeight() / 2)
+		: area.withTrimmedLeft(area.getWidth() / 2);
+	if (this->infoEditor) {
+		this->infoEditor->setBounds(infoArea);
+	}
+}
+
+void EngineDemoEditor::paint(juce::Graphics& g) {
+	/** LAF */
+	auto& laf = juce::LookAndFeel::getDefaultLookAndFeel();
+
+	/** Content Area */
+	auto area = this->getContentArea();
+	bool contentVertical = area.getHeight() >= area.getWidth();
+
+	/** Fill Area */
+	g.setColour(laf.findColour(juce::ResizableWindow::ColourIds::backgroundColourId));
+	g.fillRect(area);
+}
+
+const juce::Rectangle<int> EngineDemoEditor::getContentArea() const {
+	int width = this->getWidth(), height = this->getHeight();
+	if (width > 2 * height) {
+		width = 2 * height;
+	}
+	else if (height > 2 * width) {
+		height = 2 * width;
+	}
+	return this->getLocalBounds().withSizeKeepingCentre(width, height);
+}
--- src/EngineDemoEditor.h
+++ src/EngineDemoEditor.h
@@ -4,9 +4,15 @@
 
 class EngineDemoEditor final : public juce::AudioProcessorEditor {
 public:
-	EngineDemoEditor() = delete;
 	explicit EngineDemoEditor(juce::AudioProcessor& processor);
 
+	void resized() override;
+	void paint(juce::Graphics& g) override;
+
 private:
+	std::unique_ptr<juce::TextEditor> infoEditor = nullptr;
+
+	const juce::Rectangle<int> getContentArea() const;
+
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(EngineDemoEditor)
 };
